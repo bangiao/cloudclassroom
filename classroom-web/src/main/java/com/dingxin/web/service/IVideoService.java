91e0d85a9675d38aaeace5d414287def20901d4b
pulei2 <pulei2@lenovo.com>
2020-08-07 16:49:30 +0800

    接口注释优化

--- classroom-web/src/main/java/com/dingxin/web/service/IVideoService.java
+++ classroom-web/src/main/java/com/dingxin/web/service/IVideoService.java
@@ -15,30 +15,63 @@ import java.util.List;
  */
 public interface IVideoService extends IService<Video> {
 
+    /**
+     * 模板生产的模糊查询，目前没有使用
+     * @param data
+     * @return
+     */
     List<Video> like(Video data);
 
+    /**
+     * 视频列表查询
+     * @param query
+     * @return
+     */
     IPage<Video> listQuery(VideoListRequest query);
     /**
      * 删除课程(假删除)
      */
     void deleteVideo(List<Integer> videoIds);
     /**
-     * 删除课程(假删除删除课程的同时也会删除课程下的所有视频)
+     * 删除课程下的所有视频
      */
     void deleteCurriculumRelatedVideo(List<Integer> curriculumIds);
-
+    /**
+     * 保存视频
+     * @param video
+     */
     void saveVideoRelated(VideoInsertRequest video);
-
+    /**
+     * 更新视频对应课程的课程时长
+     * @param curriculumId
+     */
     void updateCurriculumVideoDuration(Integer curriculumId);
-
+    /**
+     * 更新视频的观看次数,视频每点击一次，次数增加 1
+     * @param videoId
+     */
     void updateCurrentVideoWatchAmount(IdRequest videoId);
-
+    /**
+     * 加在当前课程下所有有效的视频
+     * @param curriculumId
+     * @return
+     */
     List<Video> loadAllValidVideoForCurrentCurriculum(Integer curriculumId);
-
+    /**
+     * 更新当前视频对应课程的观看次数
+     * @param curriculumId
+     */
     void updateCurriculumWatchAmount(Integer curriculumId);
-
+    /**
+     * 获取当前视频详细信息
+     * @param id
+     * @return
+     */
     Video loadVideoDetails(IdRequest id);
-
+    /**
+     * 更新当前视频
+     * @param video
+     */
     void updateVideo(VideoUpdateRequest video);
 
 }
